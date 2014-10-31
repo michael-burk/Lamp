a1755861892f9d69932852498b21f520bc9e9035
Michael Burk <burk@meso.net>
2014-10-31 20:21:34 +0100

    Webworker working pretty much
    needs to be tested;

--- cleanMesh.js
+++ cleanMesh.js
@@ -2,26 +2,31 @@ self.addEventListener('message', function(e) {
 
 importScripts('js/THREE.js');  
 
+var distCounter = 0;
+
 var positions1 = e.data;
 
-var deleteArray = new Float32Array( positions1.length / 3 );	
+var deleteArray1 = new Int32Array( positions1.length / 3);
 
-var minDistance = 1000000;
+for (var i = 0; i < deleteArray1; i++) deleteArray1[i] = 0;
+
+
 var counter = 0;
 
-for(var i = 0; i < positions1.length; i+=3){  
+for(var i = 0; i < 20000; i+=3){  
 
-	
-	
-		// Threading simulation needed  
-	for(var j = 0; j < 5000; j+=3){  
+		
 
-	   var newPoint = new THREE.Vector3( positions1[ i ],	positions1[ i + 1 ] , positions1[ i + 2 ])  
+	var minDistance = 99999999999;
+		
+	for(var j = 0; j < positions1.length; j+=3){  
+
+	  var newPoint = new THREE.Vector3( positions1[ i ],	positions1[ i + 1 ] , positions1[ i + 2 ]);  
 	  
 	  var otherPoint = new THREE.Vector3( positions1[ j ],	positions1[ j + 1 ] , positions1[ j + 2 ]);
 	  var distance = new THREE.Vector3();
 	  
-	  distance.subVectors(newPoint,otherPoint);
+	  distance.subVectors(otherPoint,newPoint);
 	  var length = distance.length();
 	  if(length < minDistance && length != 0){
 	    minDistance = length;
@@ -31,22 +36,23 @@ for(var i = 0; i < positions1.length; i+=3){
 	}
 
 
-	if(minDistance >= .1){
-	  
-		//deleteArray[counter] = 1;
+	if(minDistance >= 10){
+		distCounter ++;
+		deleteArray1[counter] = 1;
+
 	  	//console.log(minDistance);
 	}
 
 	if(i % 100 == 0){
 		// self.postMessage(i);
-		console.log(i);
-		deleteArray[counter] = 1;
+		console.log(counter);
+		//deleteArray1[counter] = 1;
 	}
 
 	counter++;
-
 }
 	
-  self.postMessage(deleteArray);
+  self.postMessage(deleteArray1);
+  //self.postMessage(positions1);
 }, false);
 
